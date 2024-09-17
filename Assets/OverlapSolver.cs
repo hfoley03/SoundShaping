0179aba34ba1cbb7a349dd4a1a79b3bda92c92f9
“Harry <hfoley03@gmail.com>
2024-09-17 13:11:00 +0100

    eye and overlap

--- Assets/OverlapSolver.cs
+++ Assets/OverlapSolver.cs
@@ -13,8 +13,6 @@ public class OverlapSolver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
-
         if(transform.TryGetComponent<SmallNodeSphereInteractable>(out smallScript))
         {
             scriptToDoWorkOn = smallScript;
